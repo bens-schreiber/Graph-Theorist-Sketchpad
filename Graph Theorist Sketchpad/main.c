c8725d8f0b95e22d6cf78cf2dfb878eaa4df83c9
Ben Schreiber <bpschreiber2003@gmail.com>
2024-04-25 10:55:49 -0700

    More basic UI

--- Graph Theorist Sketchpad/main.c
+++ Graph Theorist Sketchpad/main.c
@@ -22,12 +22,15 @@ int main(void)
     StringBuffer incidenceBuffer = "";
     
     bool edgeCreationMode = false;
+    bool vertexMode = true;
+    bool edgeMode = false;
     Primitive edgeCreationModeOriginPrim;
     
     bool showBvhTree = false;
     bool showAdjMatrix = false;
     bool showIncidenceMatrix = false;
     bool showVertices = true;
+    bool showEdges = true;
     
     InitWindow(screenBoundingBox.width, screenBoundingBox.height, "raylib [core] example - basic window");
     
@@ -36,35 +39,39 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())
     {
-        if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON))
-        {
-            Vector2 mousePosition = GetMousePosition();
-            if (mousePosition.x < (graphSketchBoundingBox.width - GRAPH_VERTEX_RADIUS))
-            {
-                GraphSketch_AddVertex(gs, GetMousePosition(), graphSketchBoundingBox);
-                Graph_DumpAdjMatrix(gs->Graph, adjBuffer);
-            }
-        }
         
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && gs->BvhTree != NULL)
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
-            Rectangle mouseBoundingBox = Primitive_CreatePrimitiveWithSize(GetMousePosition(), 0, 3).BoundingBox;
-            int vi = BvhTree_CheckCollision(gs->BvhTree, mouseBoundingBox);
-            if (vi >= 0)
+            if (edgeMode && gs->BvhTree != NULL)
             {
-                if (edgeCreationMode)
+                Rectangle mouseBoundingBox = Primitive_CreatePrimitiveWithSize(GetMousePosition(), 0, 3).BoundingBox;
+                int vi = BvhTree_CheckCollision(gs->BvhTree, mouseBoundingBox);
+                if (vi >= 0)
                 {
-                    VertexIndex v1 = edgeCreationModeOriginPrim.VertexIndex;
-                    VertexIndex v2 = vi;
-                    GraphSketch_AddEdge(gs, v1, v2);
-                    
+                    if (edgeCreationMode)
+                    {
+                        VertexIndex v1 = edgeCreationModeOriginPrim.VertexIndex;
+                        VertexIndex v2 = vi;
+                        GraphSketch_AddEdge(gs, v1, v2);
+                        
+                        Graph_DumpAdjMatrix(gs->Graph, adjBuffer);
+                        Graph_DumpIncidenceMatrix(gs->Graph, incidenceBuffer);
+                        edgeCreationMode = false;
+                    } else
+                    {
+                        edgeCreationMode = true;
+                        edgeCreationModeOriginPrim = gs->IndexToPrimitiveMap[vi];
+                    }
+                }
+            }
+            
+            else if (vertexMode)
+            {
+                Vector2 mousePosition = GetMousePosition();
+                if (mousePosition.x < (graphSketchBoundingBox.width - GRAPH_VERTEX_RADIUS))
+                {
+                    GraphSketch_AddVertex(gs, GetMousePosition(), graphSketchBoundingBox);
                     Graph_DumpAdjMatrix(gs->Graph, adjBuffer);
-                    Graph_DumpIncidenceMatrix(gs->Graph, incidenceBuffer);
-                    edgeCreationMode = false;
-                } else
-                {
-                    edgeCreationMode = true;
-                    edgeCreationModeOriginPrim = gs->IndexToPrimitiveMap[vi];
                 }
             }
         }
@@ -79,18 +86,50 @@ int main(void)
         
         if (showBvhTree) BvhTree_Draw(gs->BvhTree);
         
+        if (showEdges) GraphSketch_DrawEdges(gs);
+        
         DrawRectangleRec(guiBoundingBox, Fade(LIGHTGRAY, 0.3f));
-        GuiCheckBox((Rectangle){ 600, 320, 20, 20 }, "Show BVH Tree", &showBvhTree);
-        GuiCheckBox((Rectangle){ 600, 350, 20, 20 }, "Show Adjacency Matrix", &showAdjMatrix);
-        GuiCheckBox((Rectangle){ 600, 410, 20, 20 }, "Show Incidence Matrix", &showIncidenceMatrix);
-        GuiCheckBox((Rectangle){ 600, 380, 20, 20 }, "Show Vertices", &showVertices);
+        GuiCheckBox((Rectangle){ 530, 30, 20, 20 }, "Show BVH Tree", &showBvhTree);
+        GuiCheckBox((Rectangle){ 530, 60, 20, 20 }, "Show Adjacency Matrix", &showAdjMatrix);
+        GuiCheckBox((Rectangle){ 530, 90, 20, 20 }, "Show Vertices", &showVertices);
+        GuiCheckBox((Rectangle){ 530, 120, 20, 20 }, "Show Incidence Matrix", &showIncidenceMatrix);
+        GuiCheckBox((Rectangle){ 530, 150, 20, 20 }, "Show Edges", &showEdges);
+        
+        if (GuiButton((Rectangle){ 530, 210, 140, 20 }, "Vertex Mode"))
+        {
+            vertexMode = true;
+            edgeMode = false;
+        }
+        
+        if (GuiButton((Rectangle){ 530, 240, 140, 20 }, "Edge Mode"))
+        {
+            vertexMode = false;
+            edgeMode = true;
+        }
+        
+        if (vertexMode)
+        {
+            Vector2 mousePosition = GetMousePosition();
+            if (mousePosition.x < guiBoundingBox.x)
+            {
+                DrawText("V+", mousePosition.x, mousePosition.y - 20, 20, GREEN);
+            }
+        }
+        
+        if (edgeMode)
+        {
+            Vector2 mousePosition = GetMousePosition();
+            if (mousePosition.x < guiBoundingBox.x)
+            {
+                DrawText("E+", mousePosition.x, mousePosition.y - 20, 20, GREEN);
+            }
+        }
+        
         
         if (edgeCreationMode)
         {
             DrawLineV(edgeCreationModeOriginPrim.Centroid, GetMousePosition(), BLACK);
         }
-        
-        GraphSketch_DrawEdges(gs);
         
         ClearBackground(RAYWHITE);
         
